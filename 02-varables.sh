8604fd9069782db7b4f67f3c4b85eea09a9acb1c
Varamach123 <chsrikarvarma9@gmail.com>
2024-04-16 12:20:49 +0530

    hello script

--- 02-varables.sh
+++ 02-varables.sh
@@ -1,3 +1,7 @@
-jafmsgdkfgk
-emlhflhflsd
-mflhskkhfwdlhll
+#!/bin/bash
+
+
+echo "Ramesh:: Hello Suresh, How are you?"
+echo "Suresh:: Hi ramesh, I am fine. How are you?"
+echo "Ramesh:: I am fine too. how is your work?"
+echo "Suresh:: not bad. I am thinking to upgrade to devops"
