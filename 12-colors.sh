2ec06691231f33e5fa237d82f77124326f86070e
Varamach123 <chsrikarvarma9@gmail.com>
2024-04-18 18:16:03 +0530

    hello script

---
+++ 12-colors.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+VALIDATE(){
+
+    if [ $1 -ne 0 ]
+    then 
+    echo -e  "$2 ... is  $R failure $N"
+    exit 1
+    else
+    echo  -e "$2... is $R sucess $N"
+    fi
+}
+
+
+if  [ $USERID -ne 0 ]
+then 
+echo "pls run the script with root user"
+exit 1
+else 
+echo "you are super user"
+fi
+
+
+dnf install mysql -y &>>$LOGFILE
+
+VALIDATE $? "installing mysql"
+
+dnf install git -y &>>$LOGFILE
+
+VALIDATE $? "installing git"
