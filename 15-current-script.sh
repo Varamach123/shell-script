aeacb06a9b8aa454e5257b429619955b420de642
Varamach123 <chsrikarvarma9@gmail.com>
2024-04-23 13:19:55 +0530

    hello script

--- 15-current-script.sh
+++ 15-current-script.sh
@@ -5,7 +5,8 @@ COURSE="Devops from current script"
 echo "Before calling other script, course: $COURSE"
 echo "Process id of the current shell script: $$"
 
-./16-other-script.sh
+#./16-other-script.sh
+source ./16-other-script.sh
 
 echo "after calling other script, course: $COURSE"
 
