90cca87c2b7a59bd194b7e13cff2a35a743e93fb
Varamach123 <chsrikarvarma9@gmail.com>
2024-04-24 15:48:31 +0530

    hello script

--- 19-disk-usage.sh
+++ 19-disk-usage.sh
@@ -10,9 +10,9 @@ USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
 FOLDER=$(echo $line | awk -F " " '{print $NF}')
 if [ $USAGE -ge $DISK_THRSOLD ]
 then
- MESSAGE+= " $FOLDER is more than $DISK_THRSOLD, current usage: $USAGE \n"
+ MESSAGE+="$FOLDER is more than $DISK_THRSOLD, current usage: $USAGE \n"
  fi
 done <<< $DISH_USAGE
 
-echo -e  "MESSAGE: $MESSAGE
+echo -e  "MESSAGE: $MESSAGE"
 
